3669e0b732d398f2e8b43d639ed748688d0b483e
wim glenn <hey@wimglenn.com>
2024-04-29 03:22:52 -0500

    wrap build_sdist hook. closes #13 (#16)
    * wrap build_sdist hook. closes #13
    
    * hmm
    
    * lower bound on setuptools, drop support for 3.7

--- tests/test_setuptools_ext.py
+++ tests/test_setuptools_ext.py
@@ -1,4 +1,5 @@
 import os
+import tarfile
 import zipfile
 from pathlib import Path
 
@@ -108,7 +109,7 @@ this is the second line of the README.rst file — and it has non-ascii in it
 def in_srctree(tmp_path):
     tmp_path.joinpath("README.rst").write_text(example_readme, encoding="utf8")
     prev = os.getcwd()
-    os.chdir(str(tmp_path))
+    os.chdir(tmp_path)
     try:
         yield tmp_path
     finally:
@@ -118,7 +119,7 @@ def in_srctree(tmp_path):
 def test_build_wheel_pyproject_toml(in_srctree):
     in_srctree.joinpath("pyproject.toml").write_text(pyproject_full, encoding="utf8")
     whl = setuptools_ext.build_wheel(wheel_directory=str(in_srctree))
-    with zipfile.ZipFile(str(in_srctree / whl)) as zf:
+    with zipfile.ZipFile(in_srctree / whl) as zf:
         txt = zf.read("example_proj-0.1.dist-info/METADATA").decode()
     assert txt.rstrip() == expected_metadata.rstrip()
 
@@ -127,7 +128,7 @@ def test_build_wheel_setup_cfg(in_srctree):
     in_srctree.joinpath("pyproject.toml").write_text(pyproject_minimal, encoding="utf8")
     in_srctree.joinpath("setup.cfg").write_text(example_setup_cfg, encoding="utf8")
     whl = setuptools_ext.build_wheel(wheel_directory=str(in_srctree))
-    with zipfile.ZipFile(str(in_srctree / whl)) as zf:
+    with zipfile.ZipFile(in_srctree / whl) as zf:
         txt = zf.read("example_proj-0.1.dist-info/METADATA").decode()
     assert txt.rstrip() == expected_metadata.rstrip()
 
@@ -158,7 +159,47 @@ def test_wheel_generator(in_srctree, monkeypatch):
     monkeypatch.setattr("setuptools_ext.version", lambda distribution_name: "0.1")
     in_srctree.joinpath("pyproject.toml").write_text(pyproject_full, encoding="utf8")
     whl = setuptools_ext.build_wheel(wheel_directory=str(in_srctree))
-    with zipfile.ZipFile(str(in_srctree / whl)) as zf:
+    with zipfile.ZipFile(in_srctree / whl) as zf:
         txt = zf.read("example_proj-0.1.dist-info/WHEEL").decode()
     [line] = [x for x in txt.splitlines() if x.startswith("Generator: ")]
     assert line.endswith(" + setuptools-ext (0.1)")
+
+
+# For some reason the Requires-Dist looks different for sdist build vs wheel build.
+# The requirement meaning is the same, but one or the other has been normalized.
+expected_pkginfo = """\
+Metadata-Version: 2.1
+Name: example-proj
+Version: 0.1
+Summary: example summary
+Author: Wim Glenn
+Author-email: hey@wimglenn.com
+License: MIT
+Project-URL: homepage, https://example.org/
+Description-Content-Type: text/x-rst
+Provides-Extra: all
+Requires-Dist: typing-extensions<5,>=4.2; python_version < "3.8" and extra == "all"
+Requires-External: C
+Requires-External: libpng (>=1.5)
+Requires-External: make; sys_platform != "win32"
+Supported-Platform: RedHat 8.3
+
+this is the first line of the README.rst file
+this is the second line of the README.rst file — and it has non-ascii in it
+"""
+
+
+def test_sdist(in_srctree):
+    in_srctree.joinpath("pyproject.toml").write_text(pyproject_full, encoding="utf8")
+    sdist_fname = setuptools_ext.build_sdist(sdist_directory=str(in_srctree))
+    r = 0
+    with tarfile.open(in_srctree / sdist_fname) as tf:
+        for tarinfo in tf.getmembers():
+            if tarinfo.name.endswith("/PKG-INFO"):
+                content = tf.extractfile(tarinfo).read()
+                assert content.decode() == expected_pkginfo
+                r += 1
+    # The sdists currently produced by setuptools have a PKG-INFO file both at root
+    # and within an egg-info subdirectory. I don't know why, seems like historical
+    # cruft or just a bug.
+    assert r == 1 or r == 2
